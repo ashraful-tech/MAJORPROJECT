4b161767d42de59cd970469ef6dcb630985eb18f
ashraful-tech <islam.ashraful.cse@gmail.com>
2025-04-13 16:43:26 +0600

    new commit

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review");
 
 const port = 8080;
 
@@ -38,14 +39,25 @@ app.get("/", (req, res) => {
 });
 
 //Validate Listing
-const validateListing = (req,res,next) =>{
-   let {error} = listingSchema.validate(req.body);
-   if(error){
-    let errMsg = error.details.map((el)=>{el.message}).join(",");
-    throw new ExpressError(404, errMsg);
-   } else{
-    next();
-   }
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => { el.message }).join(",");
+        throw new ExpressError(404, errMsg);
+    } else {
+        next();
+    }
+};
+
+//Validate Review
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => { el.message }).join(",");
+        throw new ExpressError(404, errMsg);
+    } else {
+        next();
+    }
 };
 
 //Index route
@@ -62,13 +74,13 @@ app.get("/listings/new", (req, res) => {
 //Show route
 app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing })
 }));
 
 //Create route
 app.post("/listings", validateListing, wrapAsync(async (req, res) => {
-    const newListing = new Listing(listing);
+    const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
 })
@@ -100,6 +112,34 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     console.log(delItem);
     res.redirect("/listings");
 }));
+
+//Reviews
+//Post review Route
+
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    console.log("new review saved");
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//Delete review route
+
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+})
+);
 
 //Error Handleing
 
